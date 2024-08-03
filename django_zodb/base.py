b54f2236254fe1085d92618cb3b92e34a396a6dd
Jeffrey Alex Clark <aclark@aclark.net>
2024-08-03 16:33:03 -0400

    Fully "functional dummy"
    We've reached NotImplementedError!
    
      File "/Users/alexclark/Developer/test/test-django-zodb/.venv/lib/python3.12/site-packages/django/db/backends/base/introspection.py", line 65, in get_table_list
        raise NotImplementedError(
    NotImplementedError: subclasses of BaseDatabaseIntrospection may require a get_table_list() method
    make: *** [django-migrate-default] Error 1

--- django_zodb/base.py
+++ django_zodb/base.py
@@ -1,26 +1,143 @@
 # django_zodb/base.py
 from django.db.backends.base.base import BaseDatabaseWrapper
+from django.db.backends.base.features import BaseDatabaseFeatures
+from django.db.backends.base.operations import BaseDatabaseOperations
+from django.db.backends.base.client import BaseDatabaseClient
+from django.db.backends.base.creation import BaseDatabaseCreation
+from django.db.backends.base.introspection import BaseDatabaseIntrospection
+from django.db.backends.base.schema import BaseDatabaseSchemaEditor
+from django.db import (
+    DatabaseError,
+    InterfaceError,
+    DataError,
+    OperationalError,
+    IntegrityError,
+    InternalError,
+    ProgrammingError,
+    NotSupportedError,
+)
+
+class MockCursor:
+    def execute(self, sql, params=None):
+        pass
+
+    def close(self):
+        pass
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
 
 class DatabaseWrapper(BaseDatabaseWrapper):
     vendor = 'zodb'
     display_name = 'ZODB'
+    client_class = BaseDatabaseClient  # Set the client_class attribute
+    creation_class = BaseDatabaseCreation  # Set the creation_class attribute
+    introspection_class = BaseDatabaseIntrospection  # Set the introspection_class attribute
+    ops_class = BaseDatabaseOperations  # Set the ops_class attribute
+    schema_editor_class = BaseDatabaseSchemaEditor  # Set the schema_editor_class attribute
+    features_class = BaseDatabaseFeatures  # Set the features_class attribute
+
+    class Database:
+        Error = DatabaseError
+        InterfaceError = InterfaceError
+        DatabaseError = DatabaseError
+        DataError = DataError
+        OperationalError = OperationalError
+        IntegrityError = IntegrityError
+        InternalError = InternalError
+        ProgrammingError = ProgrammingError
+        NotSupportedError = NotSupportedError
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.ops = self.ops_class(self)
+        self.client = self.client_class(self)
+        self.creation = self.creation_class(self)
+        self.introspection = self.introspection_class(self)
+        self.schema_editor = self.schema_editor_class(self)
+        self.features = self.features_class(self)
+        self.connection = None
+        self._in_connect = False  # Flag to indicate connection is being established
+
+    def check_settings(self):
+        # Add debug statement to trace method calls
+        print("check_settings called")
+        # Ensure this method does not call itself or any method that leads back to it
+        pass
 
     def get_connection_params(self):
-        # Return connection parameters
-        pass
+        return {}
 
     def get_new_connection(self, conn_params):
-        # Create a new connection to the database
-        pass
+        return None
 
     def init_connection_state(self):
-        # Initialize connection state
         pass
 
     def create_cursor(self, name=None):
-        # Create a new cursor
-        pass
+        return MockCursor()
 
     def _set_autocommit(self, autocommit):
-        # Set autocommit mode
+        # Avoid calling ensure_connection if already connected or in the process of connecting
+        if self.connection is None and not self._in_connect:
+            self.ensure_connection()
+        # Implement setting autocommit here
+        print(f"Setting autocommit to {autocommit}")
+        pass
+
+    def connect(self):
+        if self.connection is None:
+            self._in_connect = True
+            try:
+                self.connection = self.get_new_connection(self.get_connection_params())
+                self.init_connection_state()
+                self.set_autocommit(self.settings_dict["AUTOCOMMIT"])
+            finally:
+                self._in_connect = False
+
+    def ensure_connection(self):
+        if self.connection is None and not self._in_connect:
+            self.connect()
+
+    def _commit(self):
+        pass
+
+    def _rollback(self):
+        pass
+
+    def _savepoint(self):
+        return None
+
+    def _savepoint_rollback(self, sid):
+        pass
+
+    def _savepoint_commit(self, sid):
+        pass
+
+    def _close(self):
+        pass
+
+    class DatabaseOperations(BaseDatabaseOperations):
+        def quote_name(self, name):
+            return name
+
+        def sql_flush(self, style, tables, sequences, allow_cascade=False):
+            return []
+
+    class DatabaseClient(BaseDatabaseClient):
+        pass
+
+    class DatabaseCreation(BaseDatabaseCreation):
+        pass
+
+    class DatabaseIntrospection(BaseDatabaseIntrospection):
+        pass
+
+    class DatabaseSchemaEditor(BaseDatabaseSchemaEditor):
+        pass
+
+    class DatabaseFeatures(BaseDatabaseFeatures):
         pass
