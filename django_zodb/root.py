83b76c1c8cc15a89b84e7560ee0c2749e9f6cdc4
Jeffrey Alex Clark <aclark@aclark.net>
2024-08-04 16:46:31 -0400

    Clean up

--- django_zodb/root.py
+++ django_zodb/root.py
@@ -5,14 +5,10 @@ class FriendlyRoot:
     def __repr__(self):
         if hasattr(self.root, "_root"):
             return f"<ZODB with tables: {list(self.root._root)}>"
-        else:
-            return "<ZODB with no tables"
 
     def tables(self):
         if hasattr(self.root, "_root"):
             return list(self.root._root)
-        else:
-            return []
 
     def __getattr__(self, name):
         return getattr(self.root, name)
