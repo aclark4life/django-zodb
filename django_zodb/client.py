7ff76b1ed56304bf394e30c3336a6fb299a6b3e4
Jeffrey Alex Clark <aclark@aclark.net>
2024-08-04 07:08:10 -0400

    Start over again

--- django_zodb/client.py
+++
@@ -1,7 +0,0 @@
-# django_zodb/client.py
-from django.db.backends.base.client import BaseDatabaseClient
-
-class DatabaseClient(BaseDatabaseClient):
-    def runshell(self):
-        # Implement shell command
-        pass
