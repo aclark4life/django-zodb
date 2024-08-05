6a6d50bc638d379683548b441400742e4471888f
Jeffrey A. Clark <aclark@aclark.net>
2024-08-05 08:44:55 -0400

    Update root.py

--- django_zodb/root.py
+++ django_zodb/root.py
@@ -6,9 +6,5 @@ class FriendlyRoot:
         if hasattr(self.root, "_root"):
             return f"<ZODB with tables: {list(self.root._root)}>"
 
-    def tables(self):
-        if hasattr(self.root, "_root"):
-            return list(self.root._root)
-
     def __getattr__(self, name):
         return getattr(self.root, name)
