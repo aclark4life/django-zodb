7109cbcdd3ccb13c9e6b8ea9a25b2e41fc0fdcb4
Jeffrey A. Clark <aclark@aclark.net>
2024-10-07 14:11:04 -0400

    Custom django project

--- project.mk
+++ project.mk
@@ -1,1 +1,5 @@
 PROJECT_NAME := django-zodb
+
+django-project:
+	django-admin startproject backend . --template https://github.com/aclark4life/django-zodb-project/archive/refs/heads/main.zip 
+	-$(GIT_ADD) backend/*.py
