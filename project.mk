39d7f6550cf18ea65bbe03673ff3da9c3e1634db
Jeffrey A. Clark <aclark@aclark.net>
2024-10-07 17:16:21 -0400

    Don't git add

--- project.mk
+++ project.mk
@@ -2,7 +2,6 @@ PROJECT_NAME := django-zodb
 
 django-project:
 	django-admin startproject backend . --template https://github.com/aclark4life/django-zodb-project/archive/refs/heads/main.zip 
-	-$(GIT_ADD) backend/*.py
 
 django-settings-base:
 	@echo "$$DJANGO_SETTINGS_BASE" >> $(DJANGO_SETTINGS_BASE_FILE)
@@ -11,7 +10,6 @@ django-settings-base:
 	@echo "$$DJANGO_SETTINGS_INSTALLED_APPS" >> $(DJANGO_SETTINGS_BASE_FILE)
 	@echo "$$DJANGO_SETTINGS_MIDDLEWARE" >> $(DJANGO_SETTINGS_BASE_FILE)
 	@echo "$$DJANGO_SETTINGS_CRISPY_FORMS" >> $(DJANGO_SETTINGS_BASE_FILE)
-
 
 clean:
 	rm -rvf backend/
